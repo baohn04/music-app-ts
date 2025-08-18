9dca1ec07fd7340fac199147da6c70739fa00384
baohn04 <baohn910@gmail.com>
2025-08-18 21:59:05 +0700

    connect database

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+
+dotenv.config();
+
+database.connect();
 
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.PORT || 3000;
+
+// PUG
+app.set("views", "./views");
+app.set("view engine", "pug");
 
 app.get("/topics", (req: Request, res: Response) => {
-  res.send("Chủ đề bài hát");
+  res.render("client/pages/topics/index.pug");
 });
 
 app.listen(port, () => {
